a373fde397035f50189302e54db3ae8eb964e1f1
abb-tml <a0001150.ttl@tatamotors.com>
2025-03-25 22:38:27 +0530

    Crreate App and Tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function App(){
+  return (
+    <div className='text-3xl font-bold text-green-800'>
+      Hello WOrld
+    </div>
+  )
+}
+
+export default App;
