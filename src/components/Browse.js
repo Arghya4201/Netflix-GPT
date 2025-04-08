028cda076c6e67fa37dac70587172a4731c3f212
Arghya4201 <arghyabhowmick21@gmail.com>
2025-04-08 10:37:16 +0530

    TMDB API integration and build main container

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,12 +4,17 @@ import { API_OPTIONS } from '../utils/constants'
 import {addMovies} from '../utils/moviesSlice'
 import { useDispatch } from 'react-redux'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import MainContainer from './MainContainer'
+import SecondaryContainer from './SecondaryContainer'
+
 const Browse = () => {
   useNowPlayingMovies()
 
   return (
     <div>
       <Header/>
+      <MainContainer/>
+      <SecondaryContainer/>
       Browse
    </div>
   )
