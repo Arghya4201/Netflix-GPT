9a349c099abc85ff4bd2874d23c4478dfad3b92f
Arghya Bhowmick <arghyabhowmick21@gmail.com>
2025-04-09 08:22:46 +0530

    BgVideo CSS changes

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,8 +5,8 @@ import { useSelector } from 'react-redux'
 const SecondaryContainer = () => {
   const movies = useSelector(store => store.movies)
   return (
-    <div className='bg-black'>
-      <div className='-mt-52 pl-4'>
+    <div className='bg-black relative z-10'>
+      <div className='-mt-36 pl-4'>
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,20 +9,20 @@ const VideoBackground = ({ movieId }) => {
     useMovieTrailer(movieId)
 
     return (
-        <div className='w-screen'>
-            {/* <iframe className='w-screen aspect-video'
+        <div className='w-screen absolute'>
+            <iframe className='w-screen aspect-video h-full'
             width="560"
             height="315"
-            // src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?autoplay=1&mute=1"}
+            src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?autoplay=1&mute=1&controls=0"}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen>
-            </iframe> */}
-            <img
+            </iframe>
+            {/* <img
                 className="w-screen aspect-video object-cover"
                 src="https://image.tmdb.org/t/p/w780//yFHHfHcUgGAxziP1C3lLt0q2T4s.jpg"
                 alt="Movie Background"
-            />
+            /> */}
 
         </div>
     )
